18caaebf48f028b2645b111f91f899edddc4e623
User <User@DESKTOP-T92VF6B>
2024-10-03 00:51:19 +0300

    Add project files.

---
+++ GooglePage/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using GooglePage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GooglePage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GooglePage/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GooglePage.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
